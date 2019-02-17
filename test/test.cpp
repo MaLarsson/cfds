a76df64d8d003668704a5d1a2a61d44cfdb92f6a
Marcus Larsson <marcuslarsson_92@hotmail.com>
2019-02-17 16:55:27 +0100

    added some tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,24 @@
 #include <SmallVector.hpp>
 #include <catch2/catch.hpp>
+#include <type_traits>
 
-TEST_CASE("do stuff", "[dsa]") {
-    SmallVector<int, 4> v1{1, 2, 3, 4};
-    SmallVector<int, 0> v2;
+TEST_CASE("construction of SmallVectorImpl",
+          "[SmallVectorImpl, construction]") {
+    CHECK_FALSE(std::is_default_constructible<SmallVectorImpl<int>>::value);
+    CHECK_FALSE(std::is_copy_constructible<SmallVectorImpl<int>>::value);
+    CHECK_FALSE(std::is_move_constructible<SmallVectorImpl<int>>::value);
+}
 
-    CHECK(v1.at(0) == 1);
-    CHECK(v1.back() == 4);
-    CHECK(v2.capacity() == 0);
+TEST_CASE("SmallVector assignment", "[SmallVector, assignment]") {
+    SmallVector<int, 4> v{1, 2, 3, 4};
+    SmallVector<int, 4> v2{1, 1, 1, 1};
+    v2 = v;
+
+    CHECK(v.back() == 4);
+    CHECK(v.back() == v2.back());
+
+    SmallVector<int, 4> v3;
+    v3 = std::move(v);
+
+    CHECK(v3.back() == v2.back());
 }
