107be9b291f86b9acbd25452a0f6847645bf9187
Marcus Larsson <marcus.larsson@configura.com>
2019-03-08 17:54:32 +0100

    moved cfds::DenseSetTraits to it's own file, DenseSetTraits.hpp

--- include/SmallDenseSet.hpp
+++ include/SmallDenseSet.hpp
@@ -14,10 +14,11 @@
 
 #pragma once
 
-#include <functional>
-#include <limits>
-#include <string>
+#include "DenseSetTraits.hpp"
+
 #include <utility>
+#include <initializer_list>
+#include <cstddef>
 
 namespace meta {
 namespace detail {
@@ -76,92 +77,17 @@ namespace cfds {
 template <typename T, typename Traits = DenseSetTraits<T>>
 class SmallDenseSetImpl;
 
-template <typename T>
-struct DenseSetTraits {
-    using value_type = T;
-};
-
-// Specialization of DenseSetTrats for std::string.
-// Note that the strings "__e" and "__t" are reserved to represent the empty
-// value and the tombstone value respectively.
-template <>
-struct DenseSetTraits<std::string> {
-    using value_type = std::string;
-    static value_type getEmpty() { return "__e"; }
-    static value_type getTombstone() { return "__t"; }
-};
-
 namespace detail {
 
 template <typename T>
-struct DenseSetTraitsImpl {
-    using value_type = typename T::value_type;
-
-    // Get empty
-    template <
-        typename U = T,
-        typename std::enable_if<!meta::HasGetEmpty<U>::value>::type* = nullptr>
-    static value_type getEmpty() {
-        return std::numeric_limits<value_type>::max();
-    }
-
-    template <typename U = T, typename std::enable_if<
-                                  meta::HasGetEmpty<U>::value>::type* = nullptr>
-    static value_type getEmpty() {
-        return T::getEmpty();
-    }
-
-    // Get tombstone
-    template <typename U = T,
-              typename std::enable_if<!meta::HasGetTombstone<U>::value>::type* =
-                  nullptr>
-    static value_type getTombstone() {
-        return std::numeric_limits<value_type>::min();
-    }
-
-    template <typename U = T,
-              typename std::enable_if<meta::HasGetTombstone<U>::value>::type* =
-                  nullptr>
-    static value_type getTombstone() {
-        return T::getTombstone();
-    }
-
-    // Get hash
-    template <typename U = T, typename std::enable_if<
-                                  !meta::HasGetHash<U>::value>::type* = nullptr>
-    static std::size_t getHash(const value_type& value) {
-        return std::hash<value_type>{}(value);
-    }
-
-    template <typename U = T, typename std::enable_if<
-                                  meta::HasGetHash<U>::value>::type* = nullptr>
-    static std::size_t getHash(const value_type& value) {
-        return T::getHash(value);
-    }
-
-    // Compare
-    template <typename U = T, typename std::enable_if<
-                                  !meta::HasCompare<U>::value>::type* = nullptr>
-    static bool compare(const value_type& lhs, const value_type& rhs) {
-        return lhs == rhs;
-    }
-
-    template <typename U = T, typename std::enable_if<
-                                  meta::HasCompare<U>::value>::type* = nullptr>
-    static bool compare(const value_type& lhs, const value_type& rhs) {
-        return T::compare(lhs, rhs);
-    }
-
-    template <typename T>
-    struct SmallDenseSetAlignment {
-        SmallDenseSetImpl<T> impl;
-        typename std::aligned_storage<sizeof(T), alignof(T)>::type buffer;
-    };
+struct SmallDenseSetAlignment {
+    SmallDenseSetImpl<T> impl;
+    typename std::aligned_storage<sizeof(T), alignof(T)>::type buffer;
 };
 
 } // namespace detail
 
-template <typename T, typename Traits = DenseSetTraits<T>>
+template <typename T, typename Traits>
 class SmallDenseSetImpl {
     using Hasher = detail::DenseSetTraitsImpl<Traits>;
 
